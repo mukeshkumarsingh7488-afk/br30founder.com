7ff3c591cc3050ca09bb74780a32f90209e8be9b
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-07-15 11:12:51 +0530

    Production SEO upgrade for Mukesh Raj portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.jsx";
+import "./landing-reset.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
