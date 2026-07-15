758d37ca80068552cc1ea54e46a6b81fc42892b3
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-07-15 11:36:19 +0530

    Remove landing page css import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.jsx";
-import "./landing-reset.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
